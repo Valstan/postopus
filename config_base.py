69e9a9167a2b15dd8eac4646a3a8e46a474188c5
Валентин <123qwemmm@mail.ru>
2021-07-17 16:52:24 +0300

    config_base.py для малмыжинфо безфото приоритет изменил с 4 на 6

--- config_base.py
+++ config_base.py
@@ -4,16 +4,18 @@ from moduls.read_write.get_json import getjson
 # Не забудь указать префикс базы данных вручную
 from moduls.read_write.write_json import writejson
 
-base = getjson(bases + 'd' + fbase)
+base = getjson(bases + 'm' + fbase)
 
 # if 'aprel_links' not in base:
 #    base['aprel_links'] = []
 
-if 'Почитай Малмыж https://vk.com/baraholkaml' not in base['id']['reklama']:
-    base['id']['reklama']['Почитай Малмыж https://vk.com/baraholkaml'] = 624118736
-if 'Первый Малмыжский https://vk.com/malmiz' not in base['id']['reklama']:
-    base['id']['reklama']['Первый Малмыжский https://vk.com/malmiz'] = -86517261
-if 'Кинотеатр Апрель https://vk.com/kinozal_aprel' in base['id']['novost']:
-    del base['id']['novost']['Кинотеатр Апрель https://vk.com/kinozal_aprel']
+# if 'Почитай Малмыж https://vk.com/baraholkaml' not in base['id']['reklama']:
+#    base['id']['reklama']['Почитай Малмыж https://vk.com/baraholkaml'] = 624118736
+# if 'Первый Малмыжский https://vk.com/malmiz' not in base['id']['reklama']:
+#    base['id']['reklama']['Первый Малмыжский https://vk.com/malmiz'] = -86517261
+# if 'Кинотеатр Апрель https://vk.com/kinozal_aprel' in base['id']['novost']:
+#    del base['id']['novost']['Кинотеатр Апрель https://vk.com/kinozal_aprel']
 
-writejson(bases + 'd' + fbase, base)
+base['ruletka']['bezfoto'] = 6
+
+writejson(bases + 'm' + fbase, base)
