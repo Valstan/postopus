d3534544456cc8949e44b8fdf38235a7399f153a
Валентин <123qwemmm@mail.ru>
2021-07-18 10:30:32 +0300

    добавил в кругозор группу "Это работает"

--- config_base.py
+++ config_base.py
@@ -4,18 +4,19 @@ from moduls.read_write.get_json import getjson
 # Не забудь указать префикс базы данных вручную
 from moduls.read_write.write_json import writejson
 
-base = getjson(bases + 'd' + fbase)
+base = getjson(bases + 'm' + fbase)
 
 # if 'aprel_links' not in base:
 #    base['aprel_links'] = []
 
-# if 'Почитай Малмыж https://vk.com/baraholkaml' not in base['id']['reklama']:
-#    base['id']['reklama']['Почитай Малмыж https://vk.com/baraholkaml'] = 624118736
+if 'Это работает Наука и образование https://vk.com/etorabotaet' not in base['id']['krugozor']:
+    base['id']['krugozor']['Это работает Наука и образование https://vk.com/etorabotaet'] = -37160097
+
 # if 'Первый Малмыжский https://vk.com/malmiz' not in base['id']['reklama']:
 #    base['id']['reklama']['Первый Малмыжский https://vk.com/malmiz'] = -86517261
 # if 'Кинотеатр Апрель https://vk.com/kinozal_aprel' in base['id']['novost']:
 #    del base['id']['novost']['Кинотеатр Апрель https://vk.com/kinozal_aprel']
 
-base['ruletka']['bezfoto'] = 5
+# base['ruletka']['bezfoto'] = 5
 
-writejson(bases + 'd' + fbase, base)
+writejson(bases + 'm' + fbase, base)
