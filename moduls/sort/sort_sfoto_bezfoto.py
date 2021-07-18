a4b139e936092cd5a21b42a69a3f65b788d74c94
kuxnya <123qwemmm@mail.ru>
2021-07-18 21:05:30 +0300

    ограничил минимальный размер публикации текстовых сообщений без фото в 20 знаков

--- moduls/sort/sort_sfoto_bezfoto.py
+++ moduls/sort/sort_sfoto_bezfoto.py
@@ -3,7 +3,7 @@ from moduls.utils.avtortut import avtortut
 
 def sort_sfoto_bezfoto(msg, base):
     if 'attachments' not in msg:
-        if msg['text'] not in base['bezfoto'] and msg['text'] not in base['all_bezfoto']:
+        if len(msg['text']) > 20 and msg['text'] not in base['bezfoto'] and msg['text'] not in base['all_bezfoto']:
             base['bezfoto'].append(avtortut(msg))
         msg = []
     return msg, base
