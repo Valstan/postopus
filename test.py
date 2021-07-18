f32f2ef4087bcdde83d5405a8e0ff2b1f7cd023b
Валентин <123qwemmm@mail.ru>
2021-07-18 18:48:20 +0300

    тестировщик групп в ВК по статистике

--- test.py
+++ test.py
@@ -1,17 +1,21 @@
 import json
 import os
-from time import sleep
 from collections import Counter
+from time import sleep
 
-from config import bases, fbase
-from moduls.read_write.get_json import getjson
-from moduls.read_write.get_msg import get_msg
 from moduls.read_write.get_session_vk_api import get_session_vk_api
-from moduls.read_write.write_json import writejson
-from moduls.utils.clear_copy_history import clear_copy_history
 
-with open(os.path.join('persons.json'), 'r', encoding='utf-8') as f:
-    persons = json.load(f)
+vkapp = get_session_vk_api("79229005910", "Nitro@1941")
+group_name = 'Обо всем г.Малмыж'
+group_id = 89083141
+persons = []
+limit = 1000
+for i in range(20):
+    offset = i * limit
+    get_persons = vkapp.groups.getMembers(group_id=group_id, offset=offset, fields='city', v=5.92)['items']
+    persons.extend(get_persons)
+    print(20 - i)
+    sleep(1)
 
 deactivated = 0
 city = []
@@ -20,21 +24,14 @@ for pers in persons:
         deactivated = deactivated + 1
     if 'city' in pers:
         city.append(pers['city']['title'])
-print(deactivated)
 sort_sity = dict(Counter(city))
-sort_sity = {k: v for k, v in sorted(sort_sity.items(), key=lambda item: item[1])}
-print(sort_sity)
+len_gorod = len(sort_sity)
+sort_sity = {k: v for k, v in sorted(sort_sity.items(), key=lambda item: item[1], reverse=True)}
+result = [group_name, 'Всего подписчиков - ' + str(len(persons)), 'Мертвые души - ' + str(deactivated),
+          'Количество городов - ' + str(len_gorod), 'Города:']
+for k, v in sort_sity.items():
+    result.extend([str(v) + '-' + str(k)])
 
-'''vkapp = get_session_vk_api("79229070726", "Metro1941")
-persons = []
-limit = 1000
-for i in range(18):
-    offset = i * limit
-    get_persons = vkapp.groups.getMembers(group_id=86517261, offset=offset, fields='city', v=5.92)['items']
-    persons.extend(get_persons)
-    print(i)
-    sleep(1)
 
-with open(os.path.join('persons.json'), 'w', encoding='utf-8') as f:
-    f.write(json.dumps(persons, indent=2, ensure_ascii=False))
-'''
+with open(os.path.join(str(group_id) + '.json'), 'w', encoding='utf-8') as f:
+    f.write(json.dumps(result, indent=2, ensure_ascii=False))
