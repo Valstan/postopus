0a9212ebae166dbc38360dc4120a0f420cdabd94
Валентин <123qwemmm@mail.ru>
2021-07-25 12:30:40 +0300

    убрал рандомные указатели на каждую строку Десятки, оставил просто указательный палец

--- moduls/sort/sort_sfoto_bezfoto.py
+++ moduls/sort/sort_sfoto_bezfoto.py
@@ -1,13 +1,9 @@
-import random
-
-from config import pointer
 from moduls.utils.avtortut import avtortut
 
 
 def sort_sfoto_bezfoto(msg, base):
     if 'attachments' not in msg:
         if len(msg['text']) > 20 and msg['text'] not in base['bezfoto'] and msg['text'] not in base['all_bezfoto']:
-            random.shuffle(pointer)
-            base['bezfoto'].append(pointer[0] + ' ' + avtortut(msg))
+            base['bezfoto'].append('&#128073; ' + avtortut(msg))
         msg = []
     return msg, base
