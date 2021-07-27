fda941e8f97aef41b3c5b23d94d96871df4f8cbf
Валентин <123qwemmm@mail.ru>
2021-07-27 15:24:10 +0300

    апгрейдил утилиту очистки текста от мусорных слов и знаков препинаний

--- moduls/utils/correct_txt.py
+++ moduls/utils/correct_txt.py
@@ -2,13 +2,28 @@ from config import delete_word, delete_bad_simbol
 
 
 def correct_txt(msg):
+    text_lower = msg['text'].lower()
     for i in delete_word:
-        msg['text'] = msg['text'].replace(i, '')
+        sample = i.lower()
+        while True:
+            pos = text_lower.find(sample)
+            if pos == -1:
+                break
+            msg['text'] = msg['text'][:pos] + msg['text'][pos + len(sample):]
+            text_lower = text_lower[:pos] + text_lower[pos + len(sample):]
+            pass
+
     for i in range(6):
-        msg['text'] = msg['text'].strip(delete_bad_simbol)
-    for i in delete_word:
-        msg['text'] = msg['text'].replace(i, '')
-    for i in range(6):
+        msg['text'] = msg['text'].replace('  ', ' ')
+        msg['text'] = msg['text'].replace(' !', '!')
+        msg['text'] = msg['text'].replace(' ?', '?')
+        msg['text'] = msg['text'].replace(' ,', ',')
+        msg['text'] = msg['text'].replace(' .', '.')
+        msg['text'] = msg['text'].replace('..', '.')
+        msg['text'] = msg['text'].replace('.,', '.')
+        msg['text'] = msg['text'].replace(',.', '.')
+        msg['text'] = msg['text'].replace('.!', '!')
+        msg['text'] = msg['text'].replace(',!', '!')
         msg['text'] = msg['text'].strip(delete_bad_simbol)
 
     return msg
