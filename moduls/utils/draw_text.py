b5b043c4d70cb72248ac20ac7827697adc45d75a
Валентин <123qwemmm@mail.ru>
2021-08-09 08:09:55 +0300

    цвет наложенного текста поменял на зеленый

--- moduls/utils/draw_text.py
+++ moduls/utils/draw_text.py
@@ -4,5 +4,5 @@ from PIL import ImageDraw, ImageFont
 def draw_text(img, text, vertikal, gorizont):
     draw_text_img = ImageDraw.Draw(img)
     font = ImageFont.truetype("georgia.ttf", size=20)
-    draw_text_img.text((vertikal, gorizont), text, font=font)
+    draw_text_img.text((vertikal, gorizont), text, 'green', font=font)
     return img
