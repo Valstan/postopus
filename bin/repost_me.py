62b4f4d2e3ce0fdadfb7a0761c087436b46d38bf
Валентин <123qwemmm@mail.ru>
2021-08-10 16:36:36 +0300

    подправил репост работа с базой

--- bin/repost_me.py
+++ bin/repost_me.py
@@ -10,14 +10,14 @@ def post_me(prefix_base):
     base = getjson(bases + prefix_base + fbase)
 
     for i in repost_accounts:
-        if 'repost' not in base['links'][i]:
-            base['links']['repost'][i] = []
+        if 'repost' + i not in base['links']:
+            base['links']['repost' + i] = []
         vkapp = get_session_vk_api(change_lp(i))
         new_posts = get_msg(vkapp, conf['m']['post_group']['key'], 0, 15)
         sample_template_repost = ''
         for sample in new_posts:
             sample_template_repost = ''.join(map(str, ('wall', sample['owner_id'], '_', sample['id'])))
-            if sample_template_repost not in base['links']['repost'][i]:
+            if sample_template_repost not in base['links']['repost' + i]:
                 if conf['m']['podpisi']['heshteg']['reklama'] not in sample['text'] and \
                         conf['m']['podpisi']['heshteg']['music'] not in sample['text']:
                     break
@@ -27,9 +27,9 @@ def post_me(prefix_base):
                 vkapp.wall.repost(object=sample_template_repost)
             except:
                 pass
-            base['links']['repost'][i].append(sample_template_repost)
-            while len(base['links']['repost'][i]) > 20:
-                del base['links']['repost'][i][0]
+            base['links']['repost' + i].append(sample_template_repost)
+            while len(base['links']['repost' + i]) > 20:
+                del base['links']['repost' + i][0]
     writejson(bases + prefix_base + fbase, base)
 
 
