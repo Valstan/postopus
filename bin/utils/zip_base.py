1548397448ef8d6dbc66f1284e7dd467a830e5ab
Валентин <123qwemmm@mail.ru>
2021-08-22 18:16:37 +0300

    сменил папку создания архива баз в bases

--- bin/utils/zip_base.py
+++ bin/utils/zip_base.py
@@ -4,7 +4,7 @@ import os
 
 def zip_base():
     for folder_name in ('mi', 'dran'):
-        z = zipfile.ZipFile(folder_name + '.zip', 'w')  # Создание нового архива
+        z = zipfile.ZipFile('bases/' + folder_name + '.zip', 'w')  # Создание нового архива
         for root, dirs, files in os.walk('bases/' + folder_name):  # Список всех файлов и папок в директории folder
             for file in files:
                 z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
