bec5e5c94098219a9742ada56ee308618648af41
Валентин <123qwemmm@mail.ru>
2021-08-31 09:43:15 +0300

    ашипка ишо

--- bin/sort/sort_sfoto_bezfoto.py
+++ bin/sort/sort_sfoto_bezfoto.py
@@ -6,9 +6,9 @@ def sort_sfoto_bezfoto(session, msg):
     session = load_table(session, 'bezfoto')
     session = load_table(session, 'all_bezfoto')
     if 'attachments' not in msg:
-        if len(msg['text']) > 20 and msg['text'] not in (session['bezfoto']['list'],
-                                                         session['all_bezfoto']['list']):
-            session['bezfoto']['list'].append('&#128073; ' + avtortut(msg))
+        if len(msg['text']) > 20 and msg['text'] not in (session['bezfoto']['lip'],
+                                                         session['all_bezfoto']['lip']):
+            session['bezfoto']['lip'].append('&#128073; ' + avtortut(msg))
         msg = []
     save_table(session, 'bezfoto')
     return msg
