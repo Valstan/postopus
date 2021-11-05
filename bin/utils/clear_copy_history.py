15ea2b9fd663d5656e7640b8798c67349f7b7949
Валентин <123qwemmm@mail.ru>
2021-11-05 17:56:31 +0300

    подправил

--- bin/utils/clear_copy_history.py
+++ bin/utils/clear_copy_history.py
@@ -2,8 +2,10 @@ def clear_copy_history(msg):
 
     if 'copy_history' in msg:
         new_msg = msg['copy_history'][0]
-        new_msg['likes'] = msg['likes']
-        new_msg['views'] = msg['views']
+        if 'likes' in msg:
+            new_msg['likes'] = msg['likes']
+        if 'views' in msg:
+            new_msg['views'] = msg['views']
         return new_msg
     else:
         return msg
