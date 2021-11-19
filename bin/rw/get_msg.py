b18130775f5b7fc2bdddaea88ba889789c928577
Валентин <123qwemmm@mail.ru>
2021-11-19 17:40:14 +0300

    убрал указание версии апи

--- bin/rw/get_msg.py
+++ bin/rw/get_msg.py
@@ -1,5 +1,5 @@
 def get_msg(vkapp, group, offset=0, count=1):
-    return vkapp.wall.get(owner_id=group, count=count, offset=offset, v=5.102)['items']
+    return vkapp.wall.get(owner_id=group, count=count, offset=offset)['items']
 
 
 if __name__ == '__main__':
