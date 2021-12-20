ea9ad1fee8d107b660eec02a5ccd3b751d8ab258
Валентин <123qwemmm@mail.ru>
2021-12-20 13:07:18 +0300

    почему то дубли сохраняет

--- bin/sort/sort_sfoto_bezfoto.py
+++ bin/sort/sort_sfoto_bezfoto.py
@@ -6,8 +6,9 @@ def sort_sfoto_bezfoto(session, msg):
     session = load_table(session, 'bezfoto')
     session = load_table(session, 'all_bezfoto')
     if 'attachments' not in msg:
-        if len(msg['text']) > 20 and msg['text'] not in (session['bezfoto']['lip'],
-                                                         session['all_bezfoto']['lip']):
+        if len(msg['text']) > 20 and \
+            msg['text'] not in session['bezfoto']['lip'] and \
+            msg['text'] not in session['all_bezfoto']['lip']:
             session['bezfoto']['lip'].append('&#128073; ' + avtortut(msg))
         msg = []
     save_table(session, 'bezfoto')
