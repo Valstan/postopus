96e5419c40ac2b8c82ac3b78886b4f88afd627b1
Валентин <123qwemmm@mail.ru>
2021-12-22 16:42:35 +0300

    сделал обертку для ссылки на объявление

--- bin/utils/avtortut.py
+++ bin/utils/avtortut.py
@@ -1,6 +1,7 @@
 def avtortut(msg):
     if ' -> https://vk.com/wall' not in msg['text']:
-        return msg['text'] + ' -> https://vk.com/wall' + str(msg['owner_id']) + '_' + str(msg['id'])
+        return msg['text'] + ' -> @https://vk.com/wall' +\
+               str(msg['owner_id']) + '_' + str(msg['id']) + ' (Ответить на объявление)'
     return msg['text']
 
 
