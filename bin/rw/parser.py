b6c9560e6f2b684c04c09e381fffc7fb7208a6b7
Валентин <123qwemmm@mail.ru>
2021-12-22 16:15:08 +0300

    оптимизировал импорты

--- bin/rw/parser.py
+++ bin/rw/parser.py
@@ -2,12 +2,9 @@ import re
 
 from bin.rw.read_posts import read_posts
 from bin.sort.sort_black_list import sort_black_list
-from bin.sort.sort_double import sort_double
 from bin.sort.sort_lip import sort_lip
 from bin.sort.sort_old_date import sort_old_date
 from bin.sort.sort_po_foto import sort_po_foto
-from bin.sort.sort_sfoto_bezfoto import sort_sfoto_bezfoto
-from bin.sort.sort_views_bezfoto import sort_views_bezfoto
 from bin.utils.avtortut import avtortut
 from bin.utils.clear_copy_history import clear_copy_history
 from bin.utils.driver import load_table, save_table
