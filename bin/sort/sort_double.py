f2fef7016f123a94033f7a3af029719a4d9bdf8a
Валентин <123qwemmm@mail.ru>
2021-12-22 12:00:14 +0300

    почистил все баги древние по поиску дублей текстов

--- bin/sort/sort_double.py
+++ bin/sort/sort_double.py
@@ -4,9 +4,9 @@ from bin.utils.driver import load_table
 def sort_double(session, msg, news_msg_list, maingroup_msg_list):
     session = load_table(session, 'bezfoto')
     session = load_table(session, 'all_bezfoto')
-    if msg['text'] in (session['all_bezfoto']['lip'],
-                       session['bezfoto']['lip'],
-                       news_msg_list,
-                       maingroup_msg_list):
+    data_string = " ".join(session['bezfoto']['lip'] + session['all_bezfoto']['lip'] + news_msg_list)
+    for val in maingroup_msg_list:
+        data_string += ' ' + val['text']
+    if msg['text'] in data_string:
         msg = []
     return msg
