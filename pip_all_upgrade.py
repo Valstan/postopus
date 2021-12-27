9d46f2550691fe161158a3a93aa9a8c08139a604
Валентин <123qwemmm@mail.ru>
2021-12-27 16:54:24 +0300

    доделал вроде, запустил новый продакш на новом сервере с убунтой

--- pip_all_upgrade.py
+++ pip_all_upgrade.py
@@ -13,13 +13,13 @@ reboot
 su - valstan
 sudo -S apt update
 sudo apt upgrade
+sudo dpkg-reconfigure locales
 sudo apt install mc
 sudo apt -y install python3-pip
 sudo apt install nano
 nano .bashrc
 Добавляем в конец строку:
 export PATH=/home/valstan/.local/bin:$PATH
-sudo reboot
 (Выбрать строку установки PyTorch на сайте - https://pytorch.org/get-started/locally/#start-locally)
 pip3 install torch==1.10.1+cpu torchvision==0.11.2+cpu torchaudio==0.10.1+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 pip3 install easyocr
@@ -31,7 +31,12 @@ git config --global user.name "ubuntuvalstan"
 git config --global user.email 123qwemmm@mail.ru
 git config --global core.editor nano
 Первый раз клонировать репу на сервер:
+cd
 git clone https://github.com/Valstan/postopus
 Обновлять репу:
+cd postopus
 git pull https://github.com/Valstan/postopus
+mkdir config
+mkdir insta_photo
+EDITOR=nano crontab -e
 '''
