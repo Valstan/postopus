b8d611846ebd12d8b13cbf73b52f0e97608558a6
Валентин <123qwemmm@mail.ru>
2021-12-28 10:28:14 +0300

    вернул инсту на комп, блокируется она на сервере

--- bin/utils/draw_text.py
+++ bin/utils/draw_text.py
@@ -3,6 +3,6 @@ from PIL import ImageDraw, ImageFont
 
 def draw_text(img, text, vertikal, gorizont):
     draw_text_img = ImageDraw.Draw(img)
-    font = ImageFont.truetype("font.ttf", size=20)
+    font = ImageFont.truetype("georgia.ttf", size=20)
     draw_text_img.text((vertikal, gorizont), text, 'green', font=font)
     return img
