df2c501c054a09218a708c91d9c4b68d8e1fdab2
server <123qwemmm@mail.ru>
2022-06-09 10:31:39 +0300

    косметика2

---
+++ se_config.py
@@ -0,0 +1,15 @@
+"""BotFather, [18.05.2022 10:22]
+Here is the token for bot ValstanBot @valstan_bot:
+
+489021673:AAH7QDGmqzOMgT0W_wINvzWC1ihfljuFAKI
+
+https://t.me/test_test_test2000
+"""
+
+TELEGA_API_TOKEN = '489021673:AAH7QDGmqzOMgT0W_wINvzWC1ihfljuFAKI'  # bot @valstan_bot
+
+URL = 'https://api.telegram.org/bot'
+
+parametrs = {
+    'chat_id': -1001746966097,  # канал Тест-тест-тест2000
+}
