d5eec1030c64457ad193b3a711bd9e6bbb4a817f
server <123qwemmm@mail.ru>
2022-06-09 23:30:04 +0300

    поправочка

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 
 # ----------------- Аргументы сессии скрипта ----------------------------
-Session_mi = ['mi_novost', 'mi_repost_reklama', 'mi_addons', 'repost_brigadir',
-              'mi_repost_krugozor', 'mi_repost_aprel', 'mi_reklama']
-Session_dran = ['dran_novost', 'dran_addons', 'dran_reklama']
-Session_test = ['test_novost', 'test_addons', 'test_reklama']
-Session_valstan = ['mi_repost_me', 'repost_valstan']
-Session_insta_mi = ['mi_instagram']
+Session_mi = 'mi_novost mi_repost_reklama mi_addons repost_brigadir' \
+              'mi_repost_krugozor mi_repost_aprel mi_reklama'
+Session_dran = 'dran_novost dran_addons dran_reklama'
+Session_test = 'test_novost test_addons test_reklama'
+Session_valstan = 'mi_repost_me repost_valstan'
+Session_insta_mi = 'mi_instagram'
 # Добавил сессию? Добавь ее аргументы в "Стартовое сообщение скрипта" в "Логины и пароли" и "Выбор базы"
 
 
 # ----------------- Стартовое сообщение скрипта ----------------------------
 Start_Script_Message = f"\nEnter name session of:" \
                        f"\n1-config" \
-                       f"\n{'  '.join(Session_mi)}" \
-                       f"\n{'  '.join(Session_dran)}" \
-                       f"\n{'  '.join(Session_test)}" \
-                       f"\n{'  '.join(Session_valstan)}" \
-                       f"\n{'  '.join(Session_insta_mi)}"
+                       f"\n{Session_mi}" \
+                       f"\n{Session_dran}" \
+                       f"\n{Session_test}" \
+                       f"\n{Session_valstan}" \
+                       f"\n{Session_insta_mi}"
 
 # ----------------- Для телеграм-бота ----------------------------
 TB_url = 'https://api.telegram.org/bot'
