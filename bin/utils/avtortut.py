e43ef395db623bf8aaa365701e973178f623669d
server <123qwemmm@mail.ru>
2022-06-23 12:55:47 +0300

    пробелы вокруг ссылки должны быть видимо

--- bin/utils/avtortut.py
+++ bin/utils/avtortut.py
@@ -7,7 +7,7 @@ def avtortut(msg):
     url = 'https://vk.com/wall' + str(msg['owner_id']) + '_' + str(msg['id'])
     podpis = random.choice(session["podpisi"]["avtortut"])
     if url not in msg['text']:
-        return msg['text'] + ' @' + url + '(' + podpis + ')'
+        return msg['text'] + ' @' + url + ' (' + podpis + ')'
     return msg['text']
 
 
