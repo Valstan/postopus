a2d6c55e5eb03f24ba1670db618de2b7607532db
Валентин <123qwemmm@mail.ru>
2022-12-07 12:35:33 +0300

    небольшая переделка

--- bin/utils/avtortut.py
+++ bin/utils/avtortut.py
@@ -5,13 +5,13 @@ def avtortut(msg):
     url = 'https://vk.com/wall' + str(msg['owner_id']) + '_' + str(msg['id'])
     if url not in msg['text']:
         # podpis = random.choice(session["podpisi"]["avtortut"])
-        address = "источнике записи"
+        address = "в источнике записи"
         for name_section in session["id"]:
             for name_group in name_section.items():
                 if name_group[1] == msg['owner_id']:
                     address = name_group[0]
 
-        return msg['text'] + '\n@' + url + ' (Подробнее в ' + address + '.)\n'
+        return msg['text'] + '\n@' + url + ' (Подробнее ' + address + '.)\n'
     return msg['text']
 
 
