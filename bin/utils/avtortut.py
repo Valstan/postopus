3f8b6f7bae7d23d53925e38a5b9901a55045ba50
Валентин <123qwemmm@mail.ru>
2022-12-07 12:23:57 +0300

    изменил подпись автора источника добавил название группы, группу ищу в сессии, если ее нет, то пишется слово - источнике записи

--- bin/utils/avtortut.py
+++ bin/utils/avtortut.py
@@ -1,13 +1,17 @@
-import random
-
 from config import session
 
 
-def avtortut(msg, divider=' '):
+def avtortut(msg):
     url = 'https://vk.com/wall' + str(msg['owner_id']) + '_' + str(msg['id'])
     if url not in msg['text']:
-        podpis = random.choice(session["podpisi"]["avtortut"])
-        return msg['text'] + divider + '@' + url + ' (' + podpis + ')'
+        # podpis = random.choice(session["podpisi"]["avtortut"])
+        address = "источнике записи"
+        for name_section in session["id"]:
+            for name_group in name_section.items():
+                if name_group[1] == msg['owner_id']:
+                    address = name_group[0]
+
+        return msg['text'] + '\n@' + url + ' (Подробнее в ' + address + '.)\n'
     return msg['text']
 
 
