44661753f5cc68810a7c4955eed923d2065f880f
Валентин <123qwemmm@mail.ru>
2023-01-14 16:37:41 +0300

    сделал три уровня "давности" постов для разных типов задач

--- bin/sort/sort_old_date.py
+++ bin/sort/sort_old_date.py
@@ -7,6 +7,13 @@ def sort_old_date(sample):
 
     time_now = datetime.datetime.today().timestamp()
     difference = time_now - sample['date']
-    if difference < session['difference_old_posts']:
-        return True
+    if session['name_session'] in 'novost reklama':
+        if difference < session['time_old_post']['hard']:
+            return True
+    if session['name_session'] in 'novosti':
+        if difference < session['time_old_post']['medium']:
+            return True
+    if session['name_session'] in 'krugozor music kino prikol art':
+        if difference < session['time_old_post']['light']:
+            return True
     return False
