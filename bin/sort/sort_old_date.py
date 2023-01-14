dc88cc258de56cabf7db27827aabd718bc543440
Валентин <123qwemmm@mail.ru>
2023-01-14 16:42:07 +0300

    оптимизировал чутка и чуть не забыл Соседа в списки добавить

--- bin/sort/sort_old_date.py
+++ bin/sort/sort_old_date.py
@@ -4,16 +4,13 @@ from config import session
 
 
 def sort_old_date(sample):
-
     time_now = datetime.datetime.today().timestamp()
     difference = time_now - sample['date']
-    if session['name_session'] in 'novost reklama':
-        if difference < session['time_old_post']['hard']:
-            return True
-    if session['name_session'] in 'novosti':
-        if difference < session['time_old_post']['medium']:
-            return True
-    if session['name_session'] in 'krugozor music kino prikol art':
-        if difference < session['time_old_post']['light']:
-            return True
-    return False
+    if session['name_session'] in 'novost reklama sosed' and difference < session['time_old_post']['hard']:
+        return True
+    elif session['name_session'] in 'novosti' and difference < session['time_old_post']['medium']:
+        return True
+    elif session['name_session'] in 'krugozor music kino prikol art' and difference < session['time_old_post']['light']:
+        return True
+    else:
+        return False
