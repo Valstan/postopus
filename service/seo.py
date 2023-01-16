defb62f582f0b27282399259c5f615df8aa57a75
Валентин <123qwemmm@mail.ru>
2023-01-17 02:28:43 +0300

    ура, отшлифовал SEO для продаж клиентам услуг с отчетом. Завтра (сегодня) потестю

--- service/seo.py
+++ service/seo.py
@@ -1,5 +1,7 @@
+import os
 import random
 import time
+from datetime import datetime
 from random import shuffle
 
 from vk_api import VkApi
@@ -9,7 +11,7 @@ from bin.rw.get_attach import get_attach
 from bin.rw.get_msg import get_msg
 
 # Настройки раскрутки
-count = 30  # Сколько групп найти по каждому слову поиска
+count = 300  # Сколько групп найти по каждому слову поиска
 sleeping_min = 5  # минимальная задержка междку публикациями в секундах
 sleeping_max = 20  # максимальная задержка междку публикациями в секундах
 
@@ -21,46 +23,82 @@ session['vk_app'] = vk_session.get_api()
 
 from_group = -166980909  # Напоминашка
 
-while True:
-    rekl_posts = get_msg(from_group, 0, 100)
+count_up = 0
+count_down = 0
 
-    key_words = ["уржум", "вятские поляны", "малмыж",
-                 "кильмезь", "балтаси", "кукмор"]  # По какому слову искать сообщества
 
-    list_groups = []
-    for key in key_words:
-        list_groups.extend(session['vk_app'].groups.search(q=key, type='group', count=count)['items'])
+reklama_posts = get_msg(from_group, 0, 100)
 
-    shuffle(list_groups)
+key_words = {"уржум": 0, "вятские поляны": 0, "малмыж": 0,
+             "кильмезь": 0, "балтаси": 0, "кукмор": 0}  # По какому слову искать сообщества
 
-    for sample in list_groups:
-        number_spampost = random.randint(0, len(rekl_posts) - 1)
-        attachments = get_attach(rekl_posts[number_spampost])
+list_groups = []
+for key, value in key_words.items():
+    new_grops = session['vk_app'].groups.search(q=key, type='group', count=count)['items']
+    key_words[key] = len(new_grops)
+    list_groups.extend(new_grops)
 
-        if sample['id'] > 0:
-            sample['id'] = -sample['id']
+all_found_groups = 0
+for i in key_words:
+    all_found_groups += i
 
-        if 'can_post' in sample and sample['can_post'] == 0:
+shuffle(list_groups)
+
+list_url = ""
+
+for sample in list_groups:
+    number_spampost = random.randint(0, len(reklama_posts) - 1)
+    attachments = get_attach(reklama_posts[number_spampost])
+
+    if sample['id'] > 0:
+        sample['id'] = -sample['id']
+
+    if 'can_post' in sample and sample['can_post'] == 0:
+        continue
+    if 'wall' in sample and sample['wall'] != 1:
+        continue
+    try:
+        if sample['is_closed'] != 0 or sample['is_advertiser'] == 1 or 'deactivated' in sample:
             continue
-        if 'wall' in sample and sample['wall'] != 1:
-            continue
-        try:
-            if sample['is_closed'] != 0 or sample['is_advertiser'] == 1 or 'deactivated' in sample:
-                continue
-        except:
-            pass
+    except:
+        pass
 
-        try:
-            session['vk_app'].wall.post(owner_id=sample['id'],
-                                        from_group=0,
-                                        message=rekl_posts[number_spampost]['text'],
-                                        attachments=attachments)
-        except:
-            time.sleep(3)
-            continue
+    try:
+        session['vk_app'].wall.post(owner_id=sample['id'],
+                                    from_group=0,
+                                    message=reklama_posts[number_spampost]['text'],
+                                    attachments=attachments)
+    except:
+        count_down += 1
+        time.sleep(3)
+        continue
 
-        try:
-            print(f"{sample['name']} {sample['screen_name']}")
-        except:
-            pass
-        time.sleep(random.randint(sleeping_min, sleeping_max))
+    list_url += f"""
+    <p>https://vk.com/{sample['screen_name']}</p>"""
+    count_up += 1
+    print(f"Отработано {count_up + count_down} из {all_found_groups}. Удачно - {count_up}, Неудачно - {count_down}")
+    time.sleep(random.randint(sleeping_min, sleeping_max))
+
+result = f"""<html>
+<head>
+<title>Title</title>
+</head>
+<body>
+<h2>Список ключевых слов поиска и количество найденных по ним групп:</h2>
+<p>{key_words}</p>
+<p>Всего найдено {all_found_groups} групп.</p>
+<p>Успешно размещено {count_up} объявлений.</p>
+<p>Отказано в размещении {count_down} объявлений.</p>
+<p></p>
+<h2>Список ссылок на группы в которые удалось разместить объявление:</h2>
+<p></p>
+{list_url}
+</body>
+</html>
+"""
+
+
+with open(os.path.join(f"Спам-реклама от {datetime.now().date()}-{datetime.now().time().hour}ч-"
+                       f"{datetime.now().time().minute}м.html"),
+          'w', encoding='utf-8') as f:
+    f.write(result)
