59dfbfd04b497b6c2a9afe266adc3034a4501304
Валентин <123qwemmm@mail.ru>
2023-02-10 20:17:15 +0300

    теперь репосты будут снова запускаться во всеми пакетами кроме рекламы, но каждый токен репостит один раз и все репостят одну запись, которая итак имеет самое большое число просмотров, каждый раз группа для репостов выбирается рандомно

--- bin/control/repost_me.py
+++ bin/control/repost_me.py
@@ -1,49 +1,47 @@
 import random
 import time
-from random import shuffle
 
+from bin.rw.get_msg import get_msg
 from bin.rw.get_session_vk_api import get_session_vk_api
-from bin.rw.read_posts import read_posts
 from bin.utils.driver_tables import save_table
 from bin.utils.search_text import search_text
 from config import session
 
 
 def repost_me():
-
     # Сбор токенов
     tokens = {}
     for key in session:
         if 'VK_TOKEN_' in key and 'VK_TOKEN_DRAN' not in key:
             tokens.update({key: session[key]})
+    posts = get_msg(random.choice(list(session['all_my_groups'].values())), 0, 50)
 
-    posts = read_posts(session['all_my_groups'], 30)
-
-    shuffle(posts)
-
-    # Чистим посты от излишеств
-    clear_post = []
+    # Убираем ненужные посты
+    clear_posts = []
     for sample in posts:
-        if 'copy_history' in sample or search_text(session['repost_words_black_list'], sample['text']):
+        if 'copy_history' in sample or \
+            'views' not in sample or \
+            search_text(session['repost_words_black_list'], sample['text']) or \
+            sample['owner_id'] + sample['id'] in \
+            session['work'][session['name_session']]['lip']:
             continue
-        clear_post.append(sample)
+        clear_posts.append(sample)
 
-    shuffle(clear_post)
+    if clear_posts:
+        # Сортируем по просмотрам
+        clear_posts.sort(key=lambda x: x['views']['count'], reverse=True)
+        # Делаем паузу по Станиславскому, так как возможно мы только что до этого опубликовали свежий пост
+        time.sleep(120)
+        for session['token'] in tokens.values():
+            if get_session_vk_api():
+                session['vk_app'].wall.repost(
+                    object=''.join(map(str, ('https://vk.com/wall',
+                                             clear_posts[0]['owner_id'], '_', clear_posts[0]['id']))))
+                session['work'][session['name_session']]['lip'].append(clear_posts[0]['owner_id'] +
+                                                                       clear_posts[0]['id'])
+                time.sleep(random.randint(30, 120))
 
-    for every_time in range(random.randint(1, 3)):
-        for moder in tokens:
-            shuffle(clear_post)
-            for sample in clear_post:
-                link = ''.join(map(str, ('https://vk.com/wall', sample['owner_id'], '_', sample['id'])))
-                if link not in session['work'][session['name_session']][moder]:
-                    session['token'] = tokens[moder]
-                    if get_session_vk_api():
-                        session['vk_app'].wall.repost(object=link)
-                        session['work'][session['name_session']][moder].append(link)
-                    break
-        time.sleep(random.randint(30, 120))
-
-    save_table(session['name_session'])
+        save_table(session['name_session'])
 
 
 if __name__ == '__main__':
