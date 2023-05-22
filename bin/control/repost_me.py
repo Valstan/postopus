22589dcae43cc5677756fa9577a52b2e3b898cfe
Валентин <123qwemmm@mail.ru>
2023-05-22 18:01:46 +0300

    подправил репосты

--- bin/control/repost_me.py
+++ bin/control/repost_me.py
@@ -11,31 +11,26 @@ from config import session
 
 
 def repost_me():
+    # Делаем паузу по Станиславскому, так как возможно мы только что до этого опубликовали свежий пост
+    time.sleep(120)
 
-    for i in range(5):
-        posts = get_msg(random.choice(list(session['all_my_groups'].values())), 0, 10)
+    for i in session['all_my_groups'].values():
+        posts = get_msg(i, 0, 10)
         if posts:
-            clear_posts = []
             # Убираем ненужные посты
             for sample in posts:
-                if 'copy_history' in sample or 'views' not in sample or\
-                    search_text(session['repost_words_black_list'], sample['text']) or\
+                if 'copy_history' in sample or 'views' not in sample or \
+                    search_text(session['repost_words_black_list'], sample['text']) or \
                     lip_of_post(sample) in session['work'][session['name_session']]['lip']:
                     continue
-                clear_posts.append(sample)
 
-            if clear_posts:
-                # Сортируем по просмотрам
-                clear_posts.sort(key=lambda x: x['views']['count'], reverse=True)
-                # Делаем паузу по Станиславскому, так как возможно мы только что до этого опубликовали свежий пост
-                time.sleep(120)
                 for session['token'] in session['tokens']:
                     if get_session_vk_api():
                         session['vk_app'].wall.repost(
-                            object=''.join(map(str, (url_of_post(clear_posts[0])))))
-                        time.sleep(random.randint(30, 120))
+                            object=''.join(map(str, (url_of_post(sample)))))
+                        time.sleep(random.randint(10, 60))
 
-                session['work'][session['name_session']]['lip'].append(lip_of_post(clear_posts[0]))
+                session['work'][session['name_session']]['lip'].append(lip_of_post(sample))
                 save_table(session['name_session'])
                 break
 
