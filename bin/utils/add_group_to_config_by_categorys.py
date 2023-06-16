c5bf745279efa5f1320275ca08d492a84d641274
Валентин <123qwemmm@mail.ru>
2023-06-16 21:39:19 +0300

    дорабоатываем

--- bin/utils/add_group_to_config_by_categorys.py
+++ bin/utils/add_group_to_config_by_categorys.py
@@ -1,30 +1,52 @@
+import re
+
 from pymongo import MongoClient
+from vk_api import VkApi
 
 from config import session
 
+vk_session = VkApi(token=session['VK_TOKEN_VALSTAN'])
+vk_app = vk_session.get_api()
+client = MongoClient(session['MONGO_CLIENT'])
+mongo_base = client['postopus']
+collection = mongo_base['nolinsk']
 go_program = True
 while go_program:
-    client = MongoClient(session['MONGO_CLIENT'])
-    mongo_base = client['postopus']
-    collection = mongo_base['leb']
+
     table = collection.find_one({'title': 'config'}, {'_id': 0, 'title': 0})
 
+    name_group = ''
     id_group = 0
     while not id_group:
-        id_group = int(input(f"Введи ID группы: "))
-        for i in ('detsad', 'kultura', 'admin', 'novost', 'union', 'sport'):
-            if i in table and id_group in table[i].values():
-                print(f"Эта группа уже есть в категории {i}, набери другую группу.")
-                id_group = 0
+        url = input(f"Введи ссылку на пост в группе, которую хотите добавить в базу: ")
 
-    name_group = ''
-    while not name_group:
-        name_group = input(f"Введи ИМЯ группы: ")
+        if 'wall' in url:
+            text_list = url.split(sep="wall", maxsplit=-1)
+            text_list = text_list[1].split(sep="_", maxsplit=-1)
+            id_group = int(text_list[0])
+
+            for i in ('detsad', 'kultura', 'admin', 'novost', 'union', 'sport'):
+                if i in table and id_group in table[i].values():
+                    print(f"Эта группа уже есть в категории {i}, набери другую группу.")
+                    id_group = 0
+                    break
+
+            if id_group < 0:
+                name_group = vk_app.groups.getById(group_ids=abs(id_group),
+                                                   fields='description')[0]['name']
+            else:
+                name_group_all = vk_app.users.get(user_ids=abs(id_group),
+                                              fields='first_name,last_name')[0]
+                name_group = f"{name_group_all['first_name']} {name_group_all['last_name']}"
+
+            name_group = re.sub(r"\W", ' ', name_group, 0, re.M | re.I)
+            name_group = re.sub(r'\s+', ' ', name_group, 0, re.M)
+            name_group = re.sub(r'^\s+|\s+$', '', name_group, 0, re.M)
 
     category = 0
     while not category:
         category = int(input(f"1-detsad, 2-kultura, 3-admin, 4-novost, 5-union, 6-sport\n"
-                             f"Введи НОМЕР категории: "))
+                             f"Группа: {name_group} добавить в категорию: "))
 
     if category == 1:
         category = 'detsad'
@@ -39,11 +61,11 @@ while go_program:
     if category == 6:
         category = 'sport'
 
-    go_program = int(input(f"Для выхода из программы просто нажми Ентер."
+    go_program = input(f"Для выхода из программы просто нажми Ентер."
                        f"Группа: {name_group}, номер: {id_group} в категорию: {category}\n"
                        f"Если НЕПРАВИЛЬНО введи 100 для переделки\n"
-                       f"Если все ВЕРНО, введи 1 : "))
-    if go_program == 1:
+                       f"Если все ВЕРНО, введи 1 : ")
+    if go_program == "1":
         if category not in table:
             table[category] = {}
         table[category][name_group] = id_group
