eb24c74cba4d2e844c0800af5c83eb428223135c
Валентин <123qwemmm@mail.ru>
2023-07-09 21:47:17 +0300

    уменьшил размер таблиц по умолчанию до 30 на НОВОСТ подкрутил свой размер 100

--- bin/utils/driver_tables.py
+++ bin/utils/driver_tables.py
@@ -31,7 +31,7 @@ def save_table(name_table):
     if 'table_size' in session['work'][name_table]:
         size = session['work'][name_table]['table_size']
     else:
-        size = 40
+        size = 30
     collection = session['MONGO_BASE'][session['name_base']]
     # Изменяем размеры таблиц содержащих только списки
     for n in session['work'][name_table].keys():
